b2222afb9544a4b8ce8e2dff8d00cc926ba2f5a0
kooksk <0924wisdom@naver.com>
2020-07-14 12:22:41 +0900

    #5 Coding the UI part Three - To Do Component

--- App.js
+++ App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
-import ToDo from './ToDo'; //10
+import ToDo from './ToDo';
+
 const { width, height } = Dimensions.get('window');
 
 export default class App extends React.Component {
-  //1)
-
   state = {
-    //2)
     newTodo: '',
   };
 
   render() {
-    const { newTodo } = this.state; //4)
+    const { newTodo } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
@@ -20,16 +18,15 @@ export default class App extends React.Component {
         <Text style={styles.title}>To Do</Text>
         <View style={styles.card}>
           <TextInput style={styles.input} placeholder={'New To Do'} value={newTodo} onChangeText={this._controlNewToDo} placeholderTextColor={'#999'} returnKeyType={'done'} autoCorrect={false} />
-          <ScrollView>
+          <ScrollView contentContainerStyle={styles.toDos}>
+            {/* //5) */}
             <ToDo />
           </ScrollView>
         </View>
       </View>
     );
   }
-
   _controlNewToDo = (text) => {
-    //3)
     this.setState({
       newTodo: text,
     });
@@ -76,22 +73,13 @@ const styles = StyleSheet.create({
     borderBottomWidth: StyleSheet.hairlineWidth,
     fontSize: 25,
   },
+  toDos: {
+    //4)
+    alignItems: 'center',
+  },
 });
 
 /*
-1) 함수형 컴포넌트를 클래스형 컴포넌트로 변경 : input에 텍스트를 넣으면 화면이 변해야 하고, 이를 위해서는 state를 사용해야 함
-2) state를 작성 : 이름은 새로 작성되는 todo이므로 newTodo로 하고, 값은 비어있으니까.
-3) 새로운 function생성. 이건 이벤트에서 텍스트를 가져오므로 this.setState에 newTodo를 작성되는 text로 넣어주면 됨
-4) value값을 넣어줘야 하므로 this.state를 설정해줌
-5) value값과 onChangeText, placeholderTextColor값을 설정해줌
-+ 텍스트를 입력할때 아래에 등장하는 키보드를 [done]으로 변경하기 위해 returnKeyType을 설정해줌
-⇒ returnKeyType={'done'}으로 하면, 아이폰에서 화살표가 [완료]버튼으로 나옴
-+ 자동수정을 방지하기 위해 autoCorrect={false}로 해줌
-
-#todo element를 만들고, todo list를 만들어서 리스트를 스크롤 할 수 있어야 하고, new todo는 상단에 고정되게 스타일링 해줘야 함
-6) 스크롤을 하기 위해 ScrollView 컴포넌트를 만들어줌
-+ 이 안에 todo list가 들어가야(렌더링) 되어야 하므로, 루트위치에 새로운 파일을(ToDo.js) 만들어줌
-
-10) ToDo.js를 import해줌
-11) ScrollView에는 ToDo를 렌더링해줌
+4) toDos를 만들어서 가운데 정렬해주고 ⇒ 이걸 해주면 ToDo.js에서 작성한 with -50이 적용됨
+5) ScrollView는 props이 있는데, "contentContainerStyle"임. 얘를 작성해서 앞서 만든 toDos스타일을 적용해줌
 */
--- ToDo.js
+++ ToDo.js
@@ -1,21 +1,83 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { component } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'; //2)
 
-export default class Todo extends React.Component {
-  //7)
+const { width, height } = Dimensions.get('window');
+
+export default class ToDo extends React.Component {
+  state = {
+    isEditing: false, //1)
+    isCompleted: false, //8)
+  };
   render() {
+    const { isCompleted } = this.state;
     return (
-      <View>
-        <Text>Hello</Text>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this._toggleComplete}>
+          {/* //6) //10) */}
+          <View style={[styles.circle, isCompleted ? styles.completedCircle : styles.uncompletedCircle]} />
+        </TouchableOpacity>
+        <Text style={styles.text}>Hello Hello Hello Hello Hello</Text>
       </View>
     );
   }
+
+  _toggleComplete = () => {
+    //10)
+    this.setState((prevState) => {
+      return {
+        isCompleted: !prevState.isCompleted, //12)
+      };
+    });
+  };
 }
 
-const styles = StyleSheet.create({}); //7)
+const styles = StyleSheet.create({
+  container: {
+    //3)
+    width: width - 50,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  circle: {
+    //7)
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderWidth: 3,
+    marginRight: 20,
+  },
+  completedCircle: { borderColor: '#bbb' }, //11-1)
+  uncompletedCircle: { borderColor: '#f23657' }, //11-1)
+  text: {
+    //7)
+    fontWeight: '600',
+    fontSize: 20,
+    marginVertical: 20,
+  },
+});
 
 /*
-7) 클래스 컴포넌트로 작성해줌 : 왜냐면, 수정을 누르면 state를 수정 모드로 변경해야 하므로
-8) style을 작성해주고,
-9) 렌더링 되는 부분을 작성(View, Text)
+1) 투두앱은 두개의 states가 있음. 하나는 수정할 때, 나머지는 수정 안하고 그냥 보여주기만 할 때 : 수정했을때의 state를 작성(isEditing)해주고, 디폴트값을 false로 설정해줌
+2) 리스트를 스타일링 하기 위해, Dimensions를 import해주고 + 화면의 사이즈를 불러오기 위해 작성해줌
+3) 리스트를 스타일링 하기 위해 container를 만들고, View에 적용 
+  ⇒그런데 width값을 줘도 적용이 안됨 (App.js로 가서 ScrollView 스타일링함)
+
+6) 클릭 요소를 TouchableOpacity로 만들어줌
+7) circle과 text를 만들어서 스타일을 작성하고 적용해줌
+8) circle을 클릭하면 완성표시를 만들기 위해, state를 하나 더 작성해줌. isComplete:false
+9) 로직 관리 - 변화를 만들어야 하기 때문임(완성/미완성)
+
+10) TouchableOpacity가 onPress일때 _toggleComplete함수가 실행되도록 함
+- onPress
+: 터치가 해제 될 때 호출되지만 취소 된 경우에는 호출되지 않습니다 (예 : 응답기 잠금을 도용하는 스크롤 등).
+- onPressIn
+: onPress 이전에도 터치 가능한 요소를 누르고 호출하자마자 호출됩니다. 네트워크 요청을 할 때 유용합니다.
+- onPressOut
+: onPress 이전에도 터치가 해제 되 자마자 호출됩니다.
+https://reactnative.dev/docs/touchablewithoutfeedback#onpressin
+
+11) 완성 미완성에 따라 다른 스타일을 만들어줌 . circle에 array생성
+12) setState에 !prevState.isCompleted를 해주면, onPress에 따라 true/false값이 나오면서 스타일이 변하게 됨
 */
