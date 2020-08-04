a69aae0539aaf5cf42028c67bc5126f77f0e44aa
kooksk <0924wisdom@naver.com>
2020-08-04 13:46:33 +0900

    Updates

---
+++ precache-manifest.12172f157b1c7d10c9d84fcdd05857a6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3ddff4f6e66aef16e2b",
+    "url": "/todo-app/static/js/app.32471c6f.chunk.js"
+  },
+  {
+    "revision": "62c5b7e078c0caa2f057",
+    "url": "/todo-app/static/js/runtime~app.840d42ef.js"
+  },
+  {
+    "revision": "bb787e4b316f0b84c7f3",
+    "url": "/todo-app/static/js/2.f5b64239.chunk.js"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/todo-app/expo-service-worker.js"
+  },
+  {
+    "revision": "d30a8ee6e567ce8c98028b4290c1862e",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "3d16a3f42b9007ada65463fdd1affcbb",
+    "url": "/todo-app/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/todo-app/serve.json"
+  },
+  {
+    "revision": "a990a8c98c379b1c14de33443329d80a",
+    "url": "/todo-app/static/js/2.f5b64239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/todo-app/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/todo-app/favicon-32.png"
+  },
+  {
+    "revision": "64e462f72e9b5a1043db760bfe865c5c",
+    "url": "/todo-app/favicon.ico"
+  },
+  {
+    "revision": "8877a5dfbed43e8885e3f4df1d796794",
+    "url": "/todo-app/manifest.json"
+  }
+];
